8245afac64512444a0582167d8d012ecea7fe6da
class-demo <yamunakdk7@gmail.com>
2026-07-26 15:21:50 -0400

    Update CORS for Netlify

--- backend/app.js
+++ backend/app.js
@@ -5,23 +5,20 @@ const contactRoutes = require("./routes/contact.routes");
 
 const app = express();
 
-// Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS
 app.use(
     cors({
         origin: [
             "http://localhost:5173",
             "http://localhost:5174",
-            "https://your-portfolio-domain.vercel.app", // replace with your real frontend URL
+            "https://yamunakdk.netlify.app",
         ],
         credentials: true,
     })
 );
 
-// Routes
 app.use("/api/contact", contactRoutes);
 
 module.exports = app;
