d8b09e2a481ae3b2e1a828ead0cdc9f3c888417b
class-demo <yamunakdk7@gmail.com>
2026-07-28 11:53:49 -0400

    Update portfolio with QuickChat and AI Interview Report Generator

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,23 +1,27 @@
 export const projects = [
     {
         id: 1,
-        title: "Real-Time Chat Application",
+        title: " Chat Application",
         description:
             "A full-stack real-time chat app with authentication, online status, and instant messaging using Socket.io.",
-        tech: ["React", "Node.js", "Express", "MongoDB", "Socket.io"],
+        tech: ["React", "Node.js", "Express", "Tailwind CSS",
+               "MongoDB", "Socket.io"],
         image: "/chatapp.png",
-        github: "https://github.com/yourname/chat-app",
-        live: "https://chatapp-demo.com",
+        github: "https://github.com/yamunakdk17/Project-QuickChat",
+        live: "https://quickchat-talk.netlify.app",
     },
     {
         id: 2,
-        title: "GenAI Content Generator App",
+        title: "GenAI",
         description:
             "AI-powered application that generates blogs, summaries, and ideas using OpenAI API with a clean UI.",
-        tech: ["React", "Node.js", "Express", "OpenAI API"],
+        tech: ["React", "Node.js", "Express", "MongoDB",
+            "OpenRouter AI",
+            "SCSS",
+            "JWT Authentication"],
         image: "/genai.png",
-        github: "https://github.com/yourname/genai-app",
-        live: "https://genai-demo.com",
+        github: "https://github.com/yamunakdk17/project-GENAI",
+        live: "https://project-genai.netlify.app",
     }
 ];
 
