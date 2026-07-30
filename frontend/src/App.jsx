4c91bbbc719adaff391056cce2aa386e37ee2058
class-demo <yamunakdk7@gmail.com>
2026-07-30 13:20:16 -0400

    Fix footer bottom spacing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Contact from "./components/sections/Contact";
 import Footer from "./components/layout/Footer";
 const App =() => {
   return (
-    <div className ='min-h-screen bg-black pb-[100vh]'>
+    <div className ='min-h-screen bg-black '>
       <Navbar/>
 
 
